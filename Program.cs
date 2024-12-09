210b82ef15989c6a514339f690754ac8f9c4863f
ReggieW318 <157995070+ReggieW318@users.noreply.github.com>
2024-12-09 10:41:49 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Assignment2
+{
+    class Program
+    {
+        public static string pathInventory = Path.Combine(Directory.GetCurrentDirectory(), "Inventory.txt");
+        public static string pathTransaction = Path.Combine(Directory.GetCurrentDirectory(), "Transactions.txt");
+        static void Main(string[] args)
+        {
+            string[] Program = new string[] { "Atlanta", "Baltimore", "Chicago", "Denver", "Ely", "Fargo" };
+            int[] numbers = new int[] { 102, 215, 410, 525, 711 };
+            char[] characters = new char[] { ',' };
+            Invent[] inventories = new Inventory[6]; 
+        }
+    }
+}
