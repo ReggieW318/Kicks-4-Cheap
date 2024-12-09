dba313876f9b4ceb97059d5f2e8908f4e410c860
ReggieW318 <157995070+ReggieW318@users.noreply.github.com>
2024-12-09 10:49:19 -0500

    Add files via upload

---
+++ StoreMenu.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package storemenu;
+
+/**
+ *
+ * @author Reggie Watson
+ */
+public class StoreMenu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        StoreModel myModel = new StoreModel();
+        homepage myhomepage = new homepage();
+       myhomepage.setVisible(true);
+    }
+    
+}
---
+++ StoreModel.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package storemenu;
+
+/**
+ *
+ * @author Reggie Watson
+ */
+public class StoreModel {
+     private String item;
+
+    /**
+     * Get the value of userName
+     *
+     * @return the value of userName
+     */
+    public String getitem() {
+        return item;
+    }
+
+    /**
+     * Set the value of userName
+     *
+     * @param userName new value of userName
+     */
+    public void setitem(String item) {
+        this.item = item;
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
